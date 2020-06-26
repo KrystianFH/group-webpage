d3a916e821ccd454eb32db2f35a3ba9f0a668822
KrystianFH <krystianfrancuz@gmail.com>
2020-06-25 18:44:05 -0700

    wrapped js code into functions

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
-'use strict'
-
-alert("Hello")
-
-confirm("I am going to ask you a few questions, OK?")
 
 
+    'use strict'
 
-var favorite = prompt('What is your favorite meal of the day, breakfast, lunch, or dinner?')
-var welcome;
-if (favorite === "breakfast") {
-    welcome = "Welcome to our site! Click the Breakfast link to start with breakfast!";
-} else if (favorite = "lunch") {
-    welcome = "Welcome to our site! Click the Lunch link to start with lunch!";
-} else if (favorite = "dinner") {
-    welcome = "Welcome to our site! Click the Dinner link to start with dinner!";
-} else {
-    alert = "Sorry, that answer didn't match the selections";
-}
 
-alert(welcome)
-    // document.write(welcome);
+    function askUserName(){
+        var internalUserName = prompt('Hi there! What is your name?');
+        document.write('<h2>' + 'Welcome '+ internalUserName + '</h2>');
+        
+        return internalUserName;
+    }
+        
+    
+    function confirmContinue(userName){
+        confirm(userName + " , I am going to ask you a few questions about food");
+    }
+    
+    function preference(){
+        var preference;
+        preference = prompt('Are you a vegan?');
+        return preference
+    }
+    
+    function checkPoint(preferenceQuestion){
+        if(preferenceQuestion === 'yes'){
+            alert('No problem, all of the animal products in each recipe can be replaced with your favorite meatless alternatives!');
+        } else {
+            alert('Fantastic! Our recipes are not either!');
+        }
+    }
+
+    
+    
+    
+    var externalUserName = askUserName(); // value of internalUserName not the variable name
+    confirmContinue(externalUserName);  // this is passing an argument
+    var preferenceQuestion = preference();
+    checkPoint(preferenceQuestion);
+    
+    
+    
