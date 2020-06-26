a3bc012f3b842f7b5bbcc627c4feda225dfb0071
KrystianFH <krystianfrancuz@gmail.com>
2020-06-25 20:16:13 -0700

    tried to add in a loop to js file and failed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var ableToEat = false;
 
     function askUserName(){
         var internalUserName = prompt('Hi there! What is your name?');
@@ -22,15 +23,32 @@
     function checkPoint(preferenceQuestion){
         if(preferenceQuestion === 'yes'){
             alert('No problem, all of the animal products in each recipe can be replaced with your favorite meatless alternatives!');
+            ableToEat = false;
         } else {
             alert('Fantastic! Our recipes are not either!');
+            ableToEat = true;
         }
     }
 
+    function foodTest(){      // i++
+        var answer = 22;
+        for(var i = 0; i < 10; i = i + 1){
+            var userAnswer = prompt("Please guess a number between 1 and 100");
+            if(parseInt(userAnswer) ===  answer){
+                alert('You win the right to make all of these recipes as many times as you wish!')
+                ableToEat = true;
+                break;
+            } else{
+                alert("Wrong Answer.  Try again")
+            }
+        }
+        if(i > 9){
+            ableToEat = false;
+        }
     
     
-    
-    var externalUserName = askUserName(); // value of internalUserName not the variable name
-    confirmContinue(externalUserName);  // this is passing an argument
+    var externalUserName = askUserName();
+    confirmContinue(askUserName());  
     var preferenceQuestion = preference();
     checkPoint(preferenceQuestion);
+    foodTest();
